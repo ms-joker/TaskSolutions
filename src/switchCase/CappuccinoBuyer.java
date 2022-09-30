150e2bd21934b23989b1e78b98760cc2cb92761d
mehmet akkaya <mehmetakkaya1990@gmail.com>
2022-09-30 14:55:38 +0300

    Initial commit

---
+++ src/switchCase/CappuccinoBuyer.java
@@ -0,0 +1,32 @@
+package switchCase;
+
+public class CappuccinoBuyer {
+    public static void main(String[] args) {
+
+    }
+
+}
+/*
+	1. Create a class called  CappuccinoBuyer. A variable named Size is given, write a program that can display the price and calories of cappuccino
+			Valid sizes are tall, grande, venti and their price & calories are:
+
+						tall:
+								price is $3.69
+								90 calories
+
+						grande:
+								price is $3.99;
+								120 calories
+
+						venti:
+								price is $4.29
+								150 calories
+
+			If the size is invalid then the output should be "Invalid Size"
+
+
+			Note:
+			 	Solution 1: use if statement
+				Solution 2: use switch statement
+				Solution 2: use if & switch statements mixed
+ */
---
+++ src/switchCase/Elevator.java
@@ -0,0 +1,2 @@
+package switchCase;public class Elevator {
+}
